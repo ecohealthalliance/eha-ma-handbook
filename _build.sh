44b5569833b4d22dbcc7273d6f6d24d952080810
Noam Ross <ross@ecohealthalliance.org>
2017-12-20 17:34:41 -0500

    Fix build script

--- _build.sh
+++ _build.sh
@@ -5,5 +5,5 @@ set -x
 
 rm -rf docs
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-cp -Rf assets/reproducibility-collaboration-eha-tools.html docs/reproducibility-collaboration-eha-tools
+cp -Rf assets/reproducibility-collaboration-eha-tools.html docs/reproducibility-collaboration-eha-tools.html
 cp -Rf assets docs/
