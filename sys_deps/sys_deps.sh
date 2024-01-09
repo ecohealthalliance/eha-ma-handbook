b7fde2c723ee5006a2f1f0198df682fa20fcf857
Collin Schwantes <schwantes@ecohealthalliance.org>
2024-01-09 12:05:08 -0600

    adding system dependencies

---
+++ sys_deps/sys_deps.sh
@@ -0,0 +1,9 @@
+apt-get update && apt-get install -y --no-install-recommends \
+          libcurl4-openssl-dev \
+          libssl-dev \
+          git-crypt \
+          libudunits2-dev \
+          libgdal-dev \
+          libgeos-dev \
+          libproj-dev \
+          libglpk-dev
